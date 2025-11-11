b5f2b675e80c2e725bc209f6a13e8113daf18d70
Minh <vminh2568n@gmail.com>
2025-11-11 20:40:55 +0700

    test Form Tim Kiem Khach Hang

---
+++ QuanLyBanHang/FormTimKiemKhachHang.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace QuanLyBanHang
+{
+    partial class FormTimKiemKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblThanhPho = new System.Windows.Forms.Label();
+            this.dgvKhachHang = new System.Windows.Forms.DataGridView();
+            this.cboThanhPho = new System.Windows.Forms.ComboBox();
+            this.lblSoKhachhang = new System.Windows.Forms.Label();
+            this.txtSoLuongKhachHang = new System.Windows.Forms.TextBox();
+            this.MaKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenCty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DienThoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblThanhPho
+            // 
+            this.lblThanhPho.AutoSize = true;
+            this.lblThanhPho.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThanhPho.Location = new System.Drawing.Point(35, 35);
+            this.lblThanhPho.Name = "lblThanhPho";
+            this.lblThanhPho.Size = new System.Drawing.Size(102, 20);
+            this.lblThanhPho.TabIndex = 0;
+            this.lblThanhPho.Text = "Thành phố:";
+            // 
+            // dgvKhachHang
+            // 
+            this.dgvKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachHang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaKH,
+            this.TenCty,
+            this.DiaChi,
+            this.DienThoai});
+            this.dgvKhachHang.Location = new System.Drawing.Point(12, 105);
+            this.dgvKhachHang.Name = "dgvKhachHang";
+            this.dgvKhachHang.RowHeadersWidth = 51;
+            this.dgvKhachHang.RowTemplate.Height = 24;
+            this.dgvKhachHang.Size = new System.Drawing.Size(893, 466);
+            this.dgvKhachHang.TabIndex = 1;
+            // 
+            // cboThanhPho
+            // 
+            this.cboThanhPho.FormattingEnabled = true;
+            this.cboThanhPho.Location = new System.Drawing.Point(147, 33);
+            this.cboThanhPho.Name = "cboThanhPho";
+            this.cboThanhPho.Size = new System.Drawing.Size(166, 24);
+            this.cboThanhPho.TabIndex = 2;
+            // 
+            // lblSoKhachhang
+            // 
+            this.lblSoKhachhang.AutoSize = true;
+            this.lblSoKhachhang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoKhachhang.Location = new System.Drawing.Point(636, 37);
+            this.lblSoKhachhang.Name = "lblSoKhachhang";
+            this.lblSoKhachhang.Size = new System.Drawing.Size(138, 20);
+            this.lblSoKhachhang.TabIndex = 3;
+            this.lblSoKhachhang.Text = "Số khách hàng:";
+            // 
+            // txtSoLuongKhachHang
+            // 
+            this.txtSoLuongKhachHang.Location = new System.Drawing.Point(783, 35);
+            this.txtSoLuongKhachHang.Name = "txtSoLuongKhachHang";
+            this.txtSoLuongKhachHang.Size = new System.Drawing.Size(93, 22);
+            this.txtSoLuongKhachHang.TabIndex = 4;
+            // 
+            // MaKH
+            // 
+            this.MaKH.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MaKH.DataPropertyName = "MaKH";
+            this.MaKH.HeaderText = "Mã KH";
+            this.MaKH.MinimumWidth = 6;
+            this.MaKH.Name = "MaKH";
+            // 
+            // TenCty
+            // 
+            this.TenCty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenCty.DataPropertyName = "TenCty";
+            this.TenCty.HeaderText = "Khách Hàng";
+            this.TenCty.MinimumWidth = 6;
+            this.TenCty.Name = "TenCty";
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DiaChi.DataPropertyName = "DiaChi";
+            this.DiaChi.HeaderText = "Địa chi";
+            this.DiaChi.MinimumWidth = 6;
+            this.DiaChi.Name = "DiaChi";
+            // 
+            // DienThoai
+            // 
+            this.DienThoai.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DienThoai.DataPropertyName = "DienThoai";
+            this.DienThoai.HeaderText = "Điện thoại";
+            this.DienThoai.MinimumWidth = 6;
+            this.DienThoai.Name = "DienThoai";
+            // 
+            // FormTimKiemKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 583);
+            this.Controls.Add(this.txtSoLuongKhachHang);
+            this.Controls.Add(this.lblSoKhachhang);
+            this.Controls.Add(this.cboThanhPho);
+            this.Controls.Add(this.dgvKhachHang);
+            this.Controls.Add(this.lblThanhPho);
+            this.Name = "FormTimKiemKhachHang";
+            this.Text = "FormTimKiemKhachHang";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblThanhPho;
+        private System.Windows.Forms.DataGridView dgvKhachHang;
+        private System.Windows.Forms.ComboBox cboThanhPho;
+        private System.Windows.Forms.Label lblSoKhachhang;
+        private System.Windows.Forms.TextBox txtSoLuongKhachHang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenCty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DienThoai;
+    }
+}

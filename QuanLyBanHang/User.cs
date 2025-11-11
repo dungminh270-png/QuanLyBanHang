26a6c7c8474561bea75d0561629933afe214fbc8
Rosannryy <dungminh270@gmail.com>
2025-11-11 22:11:05 +0700

    KDT

---
+++ QuanLyBanHang/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyBanHang
+{
+    internal class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty ;
+
+        [Required]
+        public string Fullname { get;set; } = string.Empty ;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get ; set; }  =   string .Empty ; 
+    }
+}
---
+++ QuanLyBanHang/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+        }
+
+        private void đăngNhậpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var f= new Login();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDongHo.Text = DateTime.Now.ToString("dd/MM/yyyyy HH:mm:ss tt");
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

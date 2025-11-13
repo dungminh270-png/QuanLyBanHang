c65037a931960b6b4a6f19d3101be453f0f25ba7
KThoa <ntkt.28122006@gmail.com>
2025-11-13 17:01:31 +0700

    xong video 1

---
+++ QuanLyBanHang/DanhMucKhachHang.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace QuanLyBanHang
+{
+    partial class DanhMucKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvKhachHang = new System.Windows.Forms.DataGridView();
+            this.MaKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenCty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DienThoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaThanhPho = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvKhachHang
+            // 
+            this.dgvKhachHang.AllowUserToAddRows = false;
+            this.dgvKhachHang.AllowUserToDeleteRows = false;
+            this.dgvKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachHang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaKH,
+            this.TenCty,
+            this.DiaChi,
+            this.DienThoai,
+            this.MaThanhPho});
+            this.dgvKhachHang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvKhachHang.Location = new System.Drawing.Point(0, 0);
+            this.dgvKhachHang.Name = "dgvKhachHang";
+            this.dgvKhachHang.ReadOnly = true;
+            this.dgvKhachHang.RowHeadersWidth = 51;
+            this.dgvKhachHang.RowTemplate.Height = 24;
+            this.dgvKhachHang.Size = new System.Drawing.Size(965, 637);
+            this.dgvKhachHang.TabIndex = 0;
+            // 
+            // MaKH
+            // 
+            this.MaKH.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MaKH.DataPropertyName = "MaKH";
+            this.MaKH.HeaderText = "MÃ KH";
+            this.MaKH.MinimumWidth = 6;
+            this.MaKH.Name = "MaKH";
+            this.MaKH.ReadOnly = true;
+            // 
+            // TenCty
+            // 
+            this.TenCty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenCty.DataPropertyName = "TenCty";
+            this.TenCty.HeaderText = "CÔNG TY";
+            this.TenCty.MinimumWidth = 6;
+            this.TenCty.Name = "TenCty";
+            this.TenCty.ReadOnly = true;
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DiaChi.DataPropertyName = "DiaChi";
+            this.DiaChi.HeaderText = "ĐỊA CHỈ";
+            this.DiaChi.MinimumWidth = 6;
+            this.DiaChi.Name = "DiaChi";
+            this.DiaChi.ReadOnly = true;
+            // 
+            // DienThoai
+            // 
+            this.DienThoai.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DienThoai.DataPropertyName = "DienThoai";
+            this.DienThoai.HeaderText = "ĐIỆN THOẠI";
+            this.DienThoai.MinimumWidth = 6;
+            this.DienThoai.Name = "DienThoai";
+            this.DienThoai.ReadOnly = true;
+            // 
+            // MaThanhPho
+            // 
+            this.MaThanhPho.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MaThanhPho.DataPropertyName = "MaThanhPho";
+            this.MaThanhPho.HeaderText = "THÀNH PHỐ";
+            this.MaThanhPho.MinimumWidth = 6;
+            this.MaThanhPho.Name = "MaThanhPho";
+            this.MaThanhPho.ReadOnly = true;
+            // 
+            // DanhMucKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 637);
+            this.Controls.Add(this.dgvKhachHang);
+            this.Name = "DanhMucKhachHang";
+            this.Text = "DanhMucKhachHang";
+            this.Load += new System.EventHandler(this.DanhMucKhachHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvKhachHang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenCty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DienThoai;
+        private System.Windows.Forms.DataGridViewComboBoxColumn MaThanhPho;
+    }
+}
---
+++ QuanLyBanHang/DanhMucKhachHang.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang
+{
+    public partial class DanhMucKhachHang : Form
+    {
+        public DanhMucKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void DanhMucKhachHang_Load(object sender, EventArgs e)
+        {
+            LoadThanhPho(); // đổ dữ liệu vào combobox thành phố
+            LoadKhachHang(); // đổ dữ liệu vào dgvKhachHang
+
+        }
+
+        string ChuoiKetNoi = @"Server=localhost\SQLEXPRESS;Database=QLBanHang;Trusted_Connection=True;";
+        private void LoadKhachHang()
+        {
+            var connection = new SqlConnection(ChuoiKetNoi);
+            var DataAdapter = new SqlDataAdapter("SELECT * FROM KhachHang ORDER BY TenCty", connection);
+            var dtKhachHang = new DataTable();
+            DataAdapter.Fill(dtKhachHang);
+            dgvKhachHang.DataSource = dtKhachHang;
+        }
+
+        private void LoadThanhPho()
+        {
+            var connection = new SqlConnection(ChuoiKetNoi);
+            var DataAdapter = new SqlDataAdapter("SELECT * FROM ThanhPho ORDER BY TenThanhPho", connection);
+            var dtThanhPho = new DataTable();
+            DataAdapter.Fill(dtThanhPho);
+
+            //tìm combo box trong dataGridView
+            var cboThanhPho = dgvKhachHang.Columns["MaThanhPho"] as DataGridViewComboBoxColumn;
+            cboThanhPho.DataSource = dtThanhPho;
+            cboThanhPho.DisplayMember = "TenThanhPho";
+            cboThanhPho.ValueMember = "MaThanhPho";
+        }
+    }
+}

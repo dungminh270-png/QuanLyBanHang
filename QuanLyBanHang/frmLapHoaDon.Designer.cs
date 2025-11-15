e53a783fe69d8b4b1ad3ce43c0603ac467ec4b81
vanminh92 <vminh2568n@gmail.com>
2025-11-15 22:31:26 +0700

    Hóa đơn bán hàng

---
+++ QuanLyBanHang/frmLapHoaDon.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace QuanLyBanHang
+{
+    partial class frmLapHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboKhachHang = new System.Windows.Forms.ComboBox();
+            this.btnThemKH = new System.Windows.Forms.Button();
+            this.lblSanPham = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMaHD = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lblTongTien = new System.Windows.Forms.Label();
+            this.btnLuuHoaDon = new System.Windows.Forms.Button();
+            this.btnInHoaDon = new System.Windows.Forms.Button();
+            this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+            this.MaSP = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.SoLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DonGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ThanhTien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(445, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LẬP HÓA ĐƠN BÁN HÀNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Khách hàng";
+            // 
+            // cboKhachHang
+            // 
+            this.cboKhachHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboKhachHang.FormattingEnabled = true;
+            this.cboKhachHang.Location = new System.Drawing.Point(278, 121);
+            this.cboKhachHang.Name = "cboKhachHang";
+            this.cboKhachHang.Size = new System.Drawing.Size(366, 37);
+            this.cboKhachHang.TabIndex = 2;
+            // 
+            // btnThemKH
+            // 
+            this.btnThemKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemKH.Location = new System.Drawing.Point(728, 121);
+            this.btnThemKH.Name = "btnThemKH";
+            this.btnThemKH.Size = new System.Drawing.Size(142, 37);
+            this.btnThemKH.TabIndex = 3;
+            this.btnThemKH.Text = "Thêm KH";
+            this.btnThemKH.UseVisualStyleBackColor = true;
+            // 
+            // lblSanPham
+            // 
+            this.lblSanPham.AutoSize = true;
+            this.lblSanPham.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSanPham.Location = new System.Drawing.Point(87, 210);
+            this.lblSanPham.Name = "lblSanPham";
+            this.lblSanPham.Size = new System.Drawing.Size(110, 25);
+            this.lblSanPham.TabIndex = 4;
+            this.lblSanPham.Text = "Sản phẩm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(570, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Mã hóa đơn";
+            // 
+            // lblMaHD
+            // 
+            this.lblMaHD.AutoSize = true;
+            this.lblMaHD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaHD.ForeColor = System.Drawing.Color.Blue;
+            this.lblMaHD.Location = new System.Drawing.Point(751, 210);
+            this.lblMaHD.Name = "lblMaHD";
+            this.lblMaHD.Size = new System.Drawing.Size(24, 25);
+            this.lblMaHD.TabIndex = 8;
+            this.lblMaHD.Text = "0";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(570, 531);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(116, 25);
+            this.lbl.TabIndex = 9;
+            this.lbl.Text = "Tổng tiền: ";
+            // 
+            // lblTongTien
+            // 
+            this.lblTongTien.AutoSize = true;
+            this.lblTongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTongTien.ForeColor = System.Drawing.Color.Red;
+            this.lblTongTien.Location = new System.Drawing.Point(748, 531);
+            this.lblTongTien.Name = "lblTongTien";
+            this.lblTongTien.Size = new System.Drawing.Size(27, 29);
+            this.lblTongTien.TabIndex = 10;
+            this.lblTongTien.Text = "0";
+            // 
+            // btnLuuHoaDon
+            // 
+            this.btnLuuHoaDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuuHoaDon.Location = new System.Drawing.Point(146, 586);
+            this.btnLuuHoaDon.Name = "btnLuuHoaDon";
+            this.btnLuuHoaDon.Size = new System.Drawing.Size(168, 45);
+            this.btnLuuHoaDon.TabIndex = 11;
+            this.btnLuuHoaDon.Text = "Tạo mới hóa đơn";
+            this.btnLuuHoaDon.UseVisualStyleBackColor = true;
+            this.btnLuuHoaDon.Click += new System.EventHandler(this.BtnLuuHoaDon_Click);
+            // 
+            // btnInHoaDon
+            // 
+            this.btnInHoaDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInHoaDon.Location = new System.Drawing.Point(686, 586);
+            this.btnInHoaDon.Name = "btnInHoaDon";
+            this.btnInHoaDon.Size = new System.Drawing.Size(168, 45);
+            this.btnInHoaDon.TabIndex = 12;
+            this.btnInHoaDon.Text = "In hóa đơn";
+            this.btnInHoaDon.UseVisualStyleBackColor = true;
+            // 
+            // dgvHoaDon
+            // 
+            this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHoaDon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaSP,
+            this.SoLuong,
+            this.DonGia,
+            this.ThanhTien});
+            this.dgvHoaDon.Location = new System.Drawing.Point(92, 288);
+            this.dgvHoaDon.Name = "dgvHoaDon";
+            this.dgvHoaDon.RowHeadersWidth = 51;
+            this.dgvHoaDon.RowTemplate.Height = 24;
+            this.dgvHoaDon.Size = new System.Drawing.Size(778, 240);
+            this.dgvHoaDon.TabIndex = 13;
+            // 
+            // MaSP
+            // 
+            this.MaSP.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MaSP.DataPropertyName = "MaSP";
+            this.MaSP.HeaderText = "OKKKK";
+            this.MaSP.MinimumWidth = 6;
+            this.MaSP.Name = "MaSP";
+            // 
+            // SoLuong
+            // 
+            this.SoLuong.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SoLuong.DataPropertyName = "SoLuong";
+            this.SoLuong.HeaderText = "Số lượng";
+            this.SoLuong.MinimumWidth = 6;
+            this.SoLuong.Name = "SoLuong";
+            // 
+            // DonGia
+            // 
+            this.DonGia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DonGia.DataPropertyName = "DonGia";
+            this.DonGia.HeaderText = "Đơn giá";
+            this.DonGia.MinimumWidth = 6;
+            this.DonGia.Name = "DonGia";
+            // 
+            // ThanhTien
+            // 
+            this.ThanhTien.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ThanhTien.DataPropertyName = "ThanhTien";
+            this.ThanhTien.HeaderText = "Thành tiền";
+            this.ThanhTien.MinimumWidth = 6;
+            this.ThanhTien.Name = "ThanhTien";
+            // 
+            // frmLapHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 760);
+            this.Controls.Add(this.dgvHoaDon);
+            this.Controls.Add(this.btnInHoaDon);
+            this.Controls.Add(this.btnLuuHoaDon);
+            this.Controls.Add(this.lblTongTien);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.lblMaHD);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSanPham);
+            this.Controls.Add(this.btnThemKH);
+            this.Controls.Add(this.cboKhachHang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLapHoaDon";
+            this.Text = "frmLapHoaDon";
+            this.Load += new System.EventHandler(this.frmLapHoaDon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboKhachHang;
+        private System.Windows.Forms.Button btnThemKH;
+        private System.Windows.Forms.Label lblSanPham;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMaHD;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblTongTien;
+        private System.Windows.Forms.Button btnLuuHoaDon;
+        private System.Windows.Forms.Button btnInHoaDon;
+        private System.Windows.Forms.DataGridView dgvHoaDon;
+        private System.Windows.Forms.DataGridViewComboBoxColumn MaSP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoLuong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DonGia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ThanhTien;
+    }
+}
---
+++ QuanLyBanHang/frmLapHoaDon.cs
@@ -0,0 +1,174 @@
+﻿using QuanLyBanHang;
+using QuanLyBanHang.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang
+{
+    public partial class frmLapHoaDon : Form
+    {
+
+        public frmLapHoaDon()
+        {
+            InitializeComponent();
+            dgvHoaDon = new DataGridView();
+            this.Controls.Add(dgvHoaDon);
+        }
+
+        private void frmLapHoaDon_Load(object sender, EventArgs e)
+        {
+            using (var db = new QLBanHangContext())
+            {
+                cboKhachHang.DataSource = db.KhachHangs.ToList();
+                cboKhachHang.DisplayMember = "TenCty";
+                cboKhachHang.ValueMember = "MaKH";
+                cboKhachHang.SelectedIndex = -1;
+
+                // Đổ dữ liệu Sản phẩm vào cột ComboBox trên lưới
+                DataGridViewComboBoxColumn colSanPham = new DataGridViewComboBoxColumn();
+                colSanPham.Name = "MaSP";
+                colSanPham.HeaderText = "Sản phẩm";
+                colSanPham.DataSource = db.SanPhams.ToList();
+                colSanPham.DisplayMember = "TenSP";
+                colSanPham.ValueMember = "MaSP";
+                dgvHoaDon.Columns.Add(colSanPham);
+            }
+        }
+
+        private void AutoFillOrderItem(int rowIndex)
+        {
+            var cellMaSP = dgvHoaDon.Rows[rowIndex].Cells["MaSP"].Value;
+            if (cellMaSP == null) return;
+
+            int maSP = Convert.ToInt32(cellMaSP);
+
+            using (var db = new QLBanHangContext())
+            {
+                // Truy vấn LINQ lấy sản phẩm
+                var sp = db.SanPhams.FirstOrDefault(s => s.MaSP == maSP);
+
+                if (sp != null)
+                {
+                    // Điền đơn giá (cột index 2)
+                    // Lưu ý: Cột DonGia trong DB có thể là nullable, cần xử lý
+                    double donGia = sp.DonGia ?? 0;
+                    dgvHoaDon.Rows[rowIndex].Cells["DonGia"].Value = donGia;
+
+                    // Lấy số lượng (cột index 1)
+                    var cellSoLuong = dgvHoaDon.Rows[rowIndex].Cells["SoLuong"].Value;
+                    int soLuong = (cellSoLuong == null) ? 0 : Convert.ToInt32(cellSoLuong);
+
+                    // Tính thành tiền (cột index 3)
+                    dgvHoaDon.Rows[rowIndex].Cells["ThanhTien"].Value = soLuong * donGia;
+                }
+            }
+            // Tính tổng tiền cả hóa đơn hiển thị lên Label
+            CapNhatTongTien();
+        }
+    
+    private void CapNhatTongTien()
+        {
+            double tong = 0;
+            foreach (DataGridViewRow row in dgvHoaDon.Rows)
+            {
+                if (row.Cells["ThanhTien"].Value != null)
+                {
+                    tong += Convert.ToDouble(row.Cells["ThanhTien"].Value);
+                }
+            }
+            lblTongTien.Text = tong.ToString("N0"); // Định dạng số đẹp (vd: 100,000)
+        }
+
+        private void dgvHoaDon_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && (e.ColumnIndex == 0 || e.ColumnIndex == 1))
+            {
+                AutoFillOrderItem(e.RowIndex);
+            }
+        }
+
+        public void BtnLuuHoaDon_Click(object sender, EventArgs e)
+        {
+            // 1. Kiểm tra dữ liệu đầu vào
+            if (cboKhachHang.SelectedValue == null)
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng!");
+                return;
+            }
+
+            // Kiểm tra lưới có hàng nào không
+            bool coSanPham = false;
+            foreach (DataGridViewRow row in dgvHoaDon.Rows)
+            {
+                if (!row.IsNewRow && row.Cells["MaSP"].Value != null)
+                {
+                    coSanPham = true;
+                    break;
+                }
+            }
+
+            if (!coSanPham)
+            {
+                MessageBox.Show("Vui lòng chọn ít nhất 1 sản phẩm!");
+                return;
+            }
+
+            // 2. Bắt đầu Lưu bằng LINQ
+            using (var db = new QLBanHangContext())
+            {
+                try
+                {
+                    // --- B1: Tạo và Lưu Hóa Đơn (Header) ---
+                    HoaDon hd = new HoaDon();
+                    hd.MaKH = cboKhachHang.SelectedValue.ToString();
+                    hd.MaNV = 1; // Thay bằng mã nhân viên đăng nhập thực tế
+                    hd.NgayLapHD = DateTime.Now;
+                    hd.NgayNhanHang = DateTime.Now.AddDays(3); // Ví dụ ngày nhận
+
+                    db.HoaDons.Add(hd); // Đánh dấu để thêm
+                    db.SaveChanges(); // Lệnh này chạy xong, hd.MaHD sẽ tự động có giá trị mới
+
+                    // --- B2: Lưu Chi Tiết Hóa Đơn (Details) ---
+                    foreach (DataGridViewRow row in dgvHoaDon.Rows)
+                    {
+                        if (row.IsNewRow) continue;
+                        if (row.Cells["MaSP"].Value == null) continue;
+
+                        int maSP = Convert.ToInt32(row.Cells["MaSP"].Value);
+                        int soLuong = Convert.ToInt32(row.Cells["SoLuong"].Value ?? 0);
+
+                        if (soLuong > 0)
+                        {
+                            ChiTietHoaDon ct = new ChiTietHoaDon();
+                            ct.MaHD = hd.MaHD; // Lấy mã hóa đơn vừa tạo ở trên
+                            ct.MaSP = maSP;
+                            ct.SoLuong = soLuong;
+
+                            db.ChiTietHoaDons.Add(ct);
+                        }
+                    }
+
+                    db.SaveChanges(); // Lưu toàn bộ chi tiết xuống DB
+
+                    // --- B3: Hoàn tất ---
+                    MessageBox.Show($"Tạo hóa đơn thành công! Mã HĐ: {hd.MaHD}");
+                    lblMaHD.Text = hd.MaHD.ToString();
+
+                    // Có thể reset form ở đây nếu muốn
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi lưu hóa đơn: " + ex.Message);
+                }
+            }
+        }
+    }
+}
